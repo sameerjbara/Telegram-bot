a884601851a25acc8e9332528618c2399ee01315
Sameer Jbara <92097672+sameerjbara@users.noreply.github.com>
2023-09-01 18:01:10 +0300

    Update find_time.py

--- find_time.py
+++ find_time.py
@@ -1,7 +1,7 @@
 import waze_classes as waze
 
 
-def get_coords_lon_lat(start_stop, end_stop):
+def calc_2_stations(start_stop, end_stop):
     route = waze.WazeRouteCalculator(str(start_stop[0] + ' ' + start_stop[1]),
                                      str(end_stop[0] + ' ' + end_stop[1]))
     answer = route.calc_route_info()
@@ -10,9 +10,9 @@ def get_coords_lon_lat(start_stop, end_stop):
 
 def calc_rout_path(bus_stops):
     time = 0
-    distance = 0
+   
     for i in range(len(bus_stops) - 1):
-        answerT, answerD = get_coords_lon_lat(bus_stops[i], bus_stops[i + 1])
+        answerT, answerD = calc_2_stations(bus_stops[i], bus_stops[i + 1])
         time += answerT
     return time
 
